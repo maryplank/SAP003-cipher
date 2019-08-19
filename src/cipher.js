9c0794550342ed89017b75ed95818dcc0411ebb7
Mariana Ribeiro <mariana-fribeiro@live.com>
2019-08-19 17:42:46 -0300

    correctly link functions

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,12 @@
 window.cipher = {
-  // ... 
+  encode: encode,
+  decode: decode
 };
+
+function encode() {
+  alert('encode called');
+}
+
+function decode() {
+  alert('decode called');
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+function clickEncode() {
+    window.cipher.encode();
+}
+
+function clickDecode() {
+    window.cipher.decode();
+}
