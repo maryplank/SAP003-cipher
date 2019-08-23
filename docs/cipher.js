275b70312381d87668cb2c9c0da4e84bec898bcb
Mariana Ribeiro <mariana-fribeiro@live.com>
2019-08-23 16:03:21 -0300

    fix identation

--- docs/cipher.js
+++ docs/cipher.js
@@ -15,40 +15,40 @@ function encode(key, text) {
   const offset = Math.abs(key); // make key positive
 
   return text
-  .split("") // array
-  .map(char => char.charCodeAt()) // ascii
-  .map(num => {
-    if (isUpperCase(num)) {
-      return (((num + offset) - 97) % 26) + 97;
-    }
+    .split("") // array
+    .map(char => char.charCodeAt()) // ascii
+    .map(num => {
+      if (isUpperCase(num)) {
+        return (((num + offset) - 97) % 26) + 97;
+      }
     
-    if (isLowerCase(num)) {
-      return (((num + offset) - 65) % 26) + 65;
-    }
+      if (isLowerCase(num)) {
+        return (((num + offset) - 65) % 26) + 65;
+      }
   
-    return num;
-  })
-  .map(char => String.fromCharCode(char)) // back to alpha chars
-  .join(""); // back to string
+      return num;
+    })
+    .map(char => String.fromCharCode(char)) // back to alpha chars
+    .join(""); // back to string
 }
 
 function decode(key, text) {
   const offset = Math.abs(key); // make key positive
 
   return text
-  .split("") // array
-  .map(char => char.charCodeAt()) // ascii
-  .map(num => {
-    if (isUpperCase(num)) {
-      return (((num - offset) - 122) % 26) + 122;
-    }
+    .split("") // array
+    .map(char => char.charCodeAt()) // ascii
+    .map(num => {
+      if (isUpperCase(num)) {
+        return (((num - offset) - 122) % 26) + 122;
+      }
     
-    if (isLowerCase(num)) {
-      return (((num - offset) - 90) % 26) + 90;
-    }
+      if (isLowerCase(num)) {
+        return (((num - offset) - 90) % 26) + 90;
+      }
   
-    return num;
-  })
-  .map(char => String.fromCharCode(char)) // back to alpha chars
-  .join(""); // back to string
+      return num;
+    })
+    .map(char => String.fromCharCode(char)) // back to alpha chars
+    .join(""); // back to string
 }
